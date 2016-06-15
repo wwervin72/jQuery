5e5e2ec973fb6cd040df1a31951da8e8ca4efe30
wwervin72 <2411120974@qq.com>
2016-06-15 16:00:31 +0800

    complete uploadfile

--- uploadFile/js/uploadfile.js
+++ uploadFile/js/uploadfile.js
@@ -7,15 +7,18 @@
 			canDrag : true,
 			canMultiple : true,
 			success : function (fileName) {   //单个文件上传成功的回调函数
-				alert(fileName + '上传成功');
 			},
 			error : function (fileName) {     //单个文件上传失败的回调函数
-				alert(fileName + '上传失败');
 			},
 			complete : function () {  //上传完成的回调函数
-				alert('上传完毕');
 			}
 		};
+
+		//判断浏览器是否支持FileReader
+		if(!window.FileReader){
+			alert('您的浏览器不支持FileReader，请更换浏览器。');
+			return;
+		}
 
 		setting = $.extend(true, {}, defaultSetting, setting);
 		setting.width < 450 && (setting.width = 450);
@@ -77,61 +80,79 @@
 			var fileArr = [],
 			fileSize = 0,
 			_this = $(item),
-			selFileBtn = $('.file_sel .file_input .sel_file_btn input[type=file]', _this),
+			fileDrag = $('.file_sel .file_drag', _this),
+			selFileIpt = $('input[type=file]', _this),
+			selFileBtn = selFileIpt.next();
 			fileCount = $('.file_info_handle .file_info .file_count', _this),
 			fileSz = $('.file_info_handle .file_info .file_size', _this),
-			cntSelFileBtn = $('.file_info_handle .file_info input[type=file]', _this),
 			beginUpload = $('.file_info_handle .file_info .uploadfile', _this),
 			fileShow = $('.file_show', _this),
-			noDragSelFile = $('.file_show .sel_file_btn', _this),
-			noDragSelFileBtn = $('.file_show .sel_file_btn input[type=file]', _this);
-
+			noDragSelFile = $('.file_show .sel_file_btn', _this);
+			
+			//显示拖拽上传部分
 			setting.canDrag || fileShow.show();
 
 			//是否可以多选
-			if(setting.canMultiple){
-				[selFileBtn, cntSelFileBtn, noDragSelFileBtn].forEach(function (item, i) {
-					item.attr('multiple', 'multiple');
-				})
-			}
+			setting.canMultiple && selFileIpt.attr('multiple', 'multiple');
 
 			//绑定事件
-			[selFileBtn, cntSelFileBtn, noDragSelFileBtn].forEach(function (item, i) {
-				item.on('change', selFile);
-			});
+			selFileIpt.on('change', selFile);
+
+			//让按钮去触发input的click事件
+			selFileBtn.on('click', function () {  
+				$(this).prev().click();
+			})
+
+			fileDrag.on({
+				dragover : dragOver, 
+				drop : selFile
+			})
 
 			beginUpload.on('click', upLoadFile);
 
-			//拖拽
-			// function funDragHover (e) {
-			// 	e.stopPropagation();
-			// 	e.preventDefault();
-			// 	this[e.type === "dragover"? "onDragOver": "onDragLeave"].call(e.target);
-			// 	return this;
-			// }
+			
 
 			// 选择文件
-			function selFile () {
-				var files = this.files,
+			function selFile (e) {
+				e = e || window.event;
+				//阻止浏览器的默认行为
+				if(e.preventDefault){  
+					e.preventDefault();	
+				}else{
+					e.returnValue = false;
+				}
+				var files = this.files || event.dataTransfer.files,
 				src = 'img/',
 				imgSrc;
-				$(files).each(function (i, item) {
+				Array.prototype.forEach.call(files, function (item, i) {
+
+					//防止重复选择相同的文件
+					var notExist = fileArr.some(function (existFile) {
+						return existFile.name === item.name;
+					})
+					if(notExist && fileArr.length != 0){
+						return !notExist;
+					}
+
 					fileArr.push(item);
 					var fr = new FileReader();
 					fr.readAsDataURL(item);
 					fr.onload = function () {
-						if(item.type.indexOf("image") > 0){
+
+						//判断展示的文件类型
+						if(item.type.indexOf("image") > -1){
 							imgSrc = fr.result;
-						}else if(item.type.indexOf("rar") > 0){
+						}else if(item.name.indexOf("rar") > -1){
 							imgSrc = src + 'rar.png';
-						}else if(item.type.indexOf("zip") > 0){
+						}else if(item.name.indexOf("zip") > -1){
 							imgSrc = src + 'zip.png';
-						}else if(item.type.indexOf("text") > 0){
-							imgSrc = src + 'text.png';
+						}else if(item.type.indexOf("text") > -1){
+							imgSrc = src + 'txt.png';
 						}else{
 							imgSrc = src + 'file.png';
 						}
 
+						//展示选择的文件
 						var imgDom = $('<span class="img_box"><span class="up_load_success" title="上传成功"></span><span class="img_handle"><span class="file_name" title="'+ item.name +'">'+ item.name +'</span><span class="icon-bin"></span></span><img src="'+ imgSrc +'"/></span>');
 						if(setting.canDrag){
 							fileShow.css('display') === 'none' && fileShow.show();
@@ -141,11 +162,20 @@
 							noDragSelFile.before(imgDom);
 						}
 					}
-				});
+				}) 
+
+				//选择的文件的信息
 				fileCount.html(fileArr.length);
 				fileSz.html(getFileInfo());
+
 				//防止在删除了上次选择的文件后，再次选择相同的文件无效的问题。
 				this.value ='';  
+			}
+
+			//拖拽
+			function dragOver (e) {
+				var event = e || window.event;
+				event.preventDefault();
 			}
 
 			//上传文件
@@ -156,7 +186,9 @@
 				}
 				fileArr.forEach(function (item, i) {
 					var upLoadSuccess = $('.img_box').eq(i).children('.up_load_success');
-					if(upLoadSuccess.css('display') === 'block') return false;   //防止重复上传
+					
+					//防止重复上传
+					if(upLoadSuccess.css('display') === 'block') return false;   
 					var formData = new FormData();
 					formData.append('file', item);
 					$.ajax({
